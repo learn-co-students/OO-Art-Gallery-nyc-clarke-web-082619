85e9f37e2151255f86c70501ed0015b8062a5b1f
JazzyMussels <jeffmussels@gmail.com>
2019-08-31 14:04:07 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,39 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all 
+    @@all 
+  end
+
+  def self.total_experience
+    self.all.map{|art| art.years_experience}.reduce(&:+)
+  end
+
+  def self.most_prolific
+     self.all.max_by{|artist| artist.paintings.length / artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.map{|gal| gal.city}.uniq
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,27 @@
 require_relative '../config/environment.rb'
 
+met = Gallery.new('The Met', 'NYC')
+moma = Gallery.new('Moma', 'NYC')
+nathistory = Gallery.new('Natural History', 'D.C.')
+amhistory = Gallery.new('American History', 'D.C.')
+louvre = Gallery.new('The Louvre', 'Paris')
+orsay = Gallery.new('Musee de Orsay', 'Paris')
 
+pollock = Artist.new('Jackson Pollock', 3)
+dali = Artist.new('Salvador Dali', 4)
+warhol = Artist.new('Andy Warhol', 2)
+picasso = Artist.new('Pablo Picasso', 5)
+
+mast = Painting.new('Great Masturbator', 12000, dali, orsay)
+campbells = Painting.new('Soup Cans', 1000, warhol, moma)
+marilyn =  Painting.new('Marilyn Monroe', 5000, warhol, met)
+guernica = Painting.new('Guernica', 9000, picasso, louvre)
+weeping = Painting.new('Weeping Woman', 8000, picasso, orsay)
+asleep = Painting.new('Asleep', 4000, picasso, met)
+autumm = Painting.new('Autumn Rhythm', 2500, pollock, amhistory)
+summer = Painting.new('Summer Rhythm', 3000, pollock, louvre)
+spring = Painting.new('Spring Rhythm', 4000, pollock, met)
+winter = Painting.new('Winter Rhythm', 1000, pollock, nathistory)
 
 
 binding.pry
